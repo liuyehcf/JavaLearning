236fc4b601d9ec47554e58b8d6f3b4cdb885399c
liuyehcf-Win <liuyehcf@163.com>
2017-12-13 22:51:45 +0800

    2017-12-13 22:51:44

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-description=$(date +%Y)"-"$(date +%m)"-"$(date +%d)"#"$(date +%H:%M:%S)
+description=$(date +%Y)"-"$(date +%m)"-"$(date +%d)" "$(date +%H:%M:%S)
 
 git add .
 git commit -m "$description"
