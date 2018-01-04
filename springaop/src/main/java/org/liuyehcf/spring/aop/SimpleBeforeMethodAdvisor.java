9ca61bbef0f7fca6c889a6d3877fdfd76135f503
liuyehcf-mac <liuyehcf@163.com>
2018-01-04 23:32:56 +0800

    2018-01-04 23:32:56

---
+++ springaop/src/main/java/org/liuyehcf/spring/aop/SimpleBeforeMethodAdvisor.java
@@ -0,0 +1,16 @@
+package org.liuyehcf.spring.aop;
+
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.lang.Nullable;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by HCF on 2018/1/4.
+ */
+public class SimpleBeforeMethodAdvisor implements MethodBeforeAdvice {
+    @Override
+    public void before(Method method, Object[] objects, @Nullable Object o) throws Throwable {
+        System.out.println("before method advice");
+    }
+}
