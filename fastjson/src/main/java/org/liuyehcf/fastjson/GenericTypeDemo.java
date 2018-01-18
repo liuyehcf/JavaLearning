e37c0fcae9c161a9306af3a667d0515f9b6db3f1
liuyehcf-mac <liuyehcf@163.com>
2018-01-18 09:07:22 +0800

    2018-01-18 09:07:22

--- fastjson/src/main/java/org/liuyehcf/fastjson/GenericTypeDemo.java
+++ fastjson/src/main/java/org/liuyehcf/fastjson/GenericTypeDemo.java
@@ -1,6 +1,8 @@
 package org.liuyehcf.fastjson;
 
-import java.util.List;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * Created by Liuye on 2017/12/15.
@@ -8,9 +10,41 @@ import java.util.List;
 public class GenericTypeDemo<T> {
 
     public static void main(String[] args) {
-        System.out.println(
-                new MyTypeReference<List<List<String>>>() {
-                    // override nothing
-                }.getType().getTypeName());
+        ParameterizedType type = (ParameterizedType) new MyTypeReference<Map<Set<LinkedList<Collection<ArrayList<Queue<Stack<Map<String, Object>>>>>>>, Set<LinkedList<Collection<ArrayList<Queue<Stack<Map<String, Object>>>>>>>>>() {
+            // override nothing
+        }.getType();
+
+        Type rawType = type.getRawType();
+        Type[] actualTypeArguments = type.getActualTypeArguments();
+        Type ownerType = type.getOwnerType();
+
+        System.out.println("type : " + type);
+        System.out.println("rawType : " + rawType);
+        System.out.println("actualTypeArguments : " + Arrays.toString(actualTypeArguments));
+        System.out.println("ownerType : " + ownerType);
+
+
+        System.out.println("各级泛型参数");
+
+        printGenericParams(type, 0);
+    }
+
+    private static void printGenericParams(Type type, int level) {
+        System.out.println(getSpace(level) + " > " + type);
+        if (type instanceof ParameterizedType) {
+            Type[] actualTypeArguments = ((ParameterizedType) type).getActualTypeArguments();
+            for (Type actualTypeArgument : actualTypeArguments) {
+                printGenericParams(actualTypeArgument, level + 1);
+            }
+        }
+    }
+
+    private static String getSpace(int level) {
+        String space = "  ";
+        String res = "";
+        for (int i = 0; i < level; i++) {
+            res += space;
+        }
+        return res;
     }
 }
