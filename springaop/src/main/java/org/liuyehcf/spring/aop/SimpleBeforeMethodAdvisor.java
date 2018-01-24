581ba5915db8668d4a5b36b3e21c09bac7baa0dd
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-01-24 09:55:45 +0800

    2018-01-24 09:55:45

--- springaop/src/main/java/org/liuyehcf/spring/aop/SimpleBeforeMethodAdvisor.java
+++ springaop/src/main/java/org/liuyehcf/spring/aop/SimpleBeforeMethodAdvisor.java
@@ -1,7 +1,6 @@
 package org.liuyehcf.spring.aop;
 
 import org.springframework.aop.MethodBeforeAdvice;
-import org.springframework.lang.Nullable;
 
 import java.lang.reflect.Method;
 
@@ -10,7 +9,7 @@ import java.lang.reflect.Method;
  */
 public class SimpleBeforeMethodAdvisor implements MethodBeforeAdvice {
     @Override
-    public void before(Method method, Object[] objects, @Nullable Object o) throws Throwable {
+    public void before(Method method, Object[] objects, Object o) throws Throwable {
         System.out.println("before method advice");
     }
 }
