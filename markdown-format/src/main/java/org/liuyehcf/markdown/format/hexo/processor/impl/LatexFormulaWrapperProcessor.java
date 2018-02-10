d77e58d27bbe0ce7f96910dfb47d8a52a71e291d
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-02-11 01:27:56 +0800

    重构LatexFormulaWrapperProcessor

--- markdown-format/src/main/java/org/liuyehcf/markdown/format/hexo/processor/impl/LatexFormulaWrapperProcessor.java
+++ markdown-format/src/main/java/org/liuyehcf/markdown/format/hexo/processor/impl/LatexFormulaWrapperProcessor.java
@@ -6,14 +6,12 @@ import org.liuyehcf.markdown.format.hexo.context.LineIterator;
 import org.liuyehcf.markdown.format.hexo.processor.AbstractFileProcessor;
 import org.liuyehcf.markdown.format.hexo.processor.PreFileProcessor;
 
+import java.io.File;
+
 import static org.liuyehcf.markdown.format.hexo.constant.StringConstant.*;
 import static org.liuyehcf.markdown.format.hexo.log.DefaultLogger.DEFAULT_LOGGER;
 
 public class LatexFormulaWrapperProcessor extends AbstractFileProcessor implements PreFileProcessor {
-    private boolean isInnerFormulaStart;
-    private boolean isInterFormulaStart;
-    private int innerFormulaCount;
-    private int interFormulaCount;
 
     @Override
     protected void doProcess(FileContext fileContext, LineIterator iterator) {
@@ -24,9 +22,9 @@ public class LatexFormulaWrapperProcessor extends AbstractFileProcessor implemen
                 && !lineElement.isCode()) {
             String content = lineElement.getContent();
 
-            content = processInnerFormula(fileContext, iterator, content);
+            content = new InnerFormulaWrapper(fileContext.getFile(), content).format();
 
-            content = processInterFormula(fileContext, iterator, content);
+            content = new InterFormulaWrapper(fileContext.getFile(), content).format();
 
             lineElement.setContent(content);
         }
@@ -34,17 +32,18 @@ public class LatexFormulaWrapperProcessor extends AbstractFileProcessor implemen
 
     @Override
     protected void beforeProcess(FileContext fileContext) {
-        isInnerFormulaStart = true;
-        isInterFormulaStart = true;
-        innerFormulaCount = 0;
-        interFormulaCount = 0;
+        InnerFormulaWrapper.isMatchLeft = true;
+        InterFormulaWrapper.isMatchLeft = true;
+        InnerFormulaWrapper.formulaCount = 0;
+        InterFormulaWrapper.formulaCount = 0;
+        InnerFormulaWrapper.isCrossing = false;
     }
 
     @Override
     protected void afterProcess(FileContext fileContext) {
-        if ((innerFormulaCount & 1) != 0
-                || (interFormulaCount & 1) != 0) {
-            DEFAULT_LOGGER.error("file [{}] contains unmatched inter formula wrappers!", fileContext.getFile());
+        if ((InnerFormulaWrapper.formulaCount & 1) != 0
+                || (InterFormulaWrapper.formulaCount & 1) != 0) {
+            DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: Inner or inter formula wrapper is not matched!!", fileContext.getFile());
         }
     }
 
@@ -64,65 +63,51 @@ public class LatexFormulaWrapperProcessor extends AbstractFileProcessor implemen
         return false;
     }
 
-    private String processInnerFormula(FileContext fileContext, LineIterator iterator, String content) {
-        int cursor = 0;
+    private static abstract class AbstractFormulaWrapper {
+        protected int cursor;
+        protected final File file;
+        protected final String content;
+        protected StringBuilder sb;
 
-        StringBuilder sb = new StringBuilder();
-
-        while (cursor < content.length()) {
-
-            // escape inner line code
-            if (content.charAt(cursor) == '`') {
-                // '`' is not allowed in formula
-                if (!isInnerFormulaStart) {
-                    DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: '`' is not allowed in formula. line content: {}", fileContext.getFile(), content);
-                    throw new RuntimeException();
-                }
-
-                sb.append(content.charAt(cursor++));
-
-                while (content.charAt(cursor) != '`') {
-                    sb.append(content.charAt(cursor++));
-                }
-
-                sb.append(content.charAt(cursor++));
-            } else if (isInnerFormulaBoundary(content, cursor)) {
-                if (isInnerFormulaStart) {
-                    if (hasFormulaStart(content, cursor)) {
-                        sb.append("$");
-                    } else {
-                        sb.append(FORMULA_WRAPPER_START)
-                                .append("$");
-                    }
-                } else {
-                    if (hasFormulaEnd(content, cursor)) {
-                        sb.append("$");
-                    } else {
-                        sb.append("$")
-                                .append(FORMULA_WRAPPER_END);
-                    }
-                }
-                isInnerFormulaStart = !isInnerFormulaStart;
-                innerFormulaCount++;
-                cursor++;
-            } else {
-                sb.append(content.charAt(cursor++));
-            }
+        public AbstractFormulaWrapper(File file, String content) {
+            cursor = 0;
+            this.file = file;
+            this.content = content;
+            sb = new StringBuilder();
         }
 
-        return sb.toString();
-    }
+        protected abstract String format();
 
-    /**
-     * 当前符号是否是一个行内公式的边界符号
-     *
-     * @param content
-     * @param cursor
-     * @return
-     */
-    private boolean isInnerFormulaBoundary(String content, int cursor) {
-        if (content.charAt(cursor) == '$'
-                && (cursor == content.length() - 1 || content.charAt(cursor + 1) != '$')) {
+        /**
+         * 是否已经包含了{% raw %}
+         *
+         * @return
+         */
+        protected final boolean hasFormulaStart() {
+            int len = FORMULA_WRAPPER_START.length();
+            return cursor >= len
+                    && FORMULA_WRAPPER_START.equals(content.substring(cursor - len, cursor));
+        }
+
+        /**
+         * 是否已经包含了{% endraw %}
+         *
+         * @return
+         */
+        protected final boolean hasFormulaEnd(int cursor) {
+            int len = FORMULA_WRAPPER_END.length();
+            return cursor + len < content.length()
+                    && FORMULA_WRAPPER_END.equals(content.substring(cursor + 1, cursor + len + 1));
+        }
+
+        /**
+         * 是否为边界的前缀
+         * 前面不可以是转义字符"\"
+         * 前面不可以是正常的$
+         *
+         * @return
+         */
+        protected final boolean isPrefixBoundary() {
             if (cursor == 0) {
                 return true;
             } else if (cursor == 1) {
@@ -131,101 +116,156 @@ public class LatexFormulaWrapperProcessor extends AbstractFileProcessor implemen
                 return content.charAt(cursor - 1) != '\\' && !(content.charAt(cursor - 1) == '$' && content.charAt(cursor - 2) != '\\');
             }
         }
-        return false;
     }
 
-    /**
-     * 是否已经包含了{% raw %}
-     *
-     * @param content
-     * @param cursor
-     * @return
-     */
-    private boolean hasFormulaStart(String content, int cursor) {
-        int len = FORMULA_WRAPPER_START.length();
-        return cursor >= len
-                && FORMULA_WRAPPER_START.equals(content.substring(cursor - len, cursor));
-    }
+    private static final class InnerFormulaWrapper extends AbstractFormulaWrapper {
+        private static boolean isMatchLeft;
+        private static int formulaCount;
+        private static boolean isCrossing;
 
-    /**
-     * 是否已经包含了{% endraw %}
-     *
-     * @param content
-     * @param cursor
-     * @return
-     */
-    private boolean hasFormulaEnd(String content, int cursor) {
-        int len = FORMULA_WRAPPER_END.length();
-        return cursor + len < content.length()
-                && FORMULA_WRAPPER_END.equals(content.substring(cursor + 1, cursor + len + 1));
-    }
+        public InnerFormulaWrapper(File file, String content) {
+            super(file, content);
+        }
 
-    private boolean isCrossLine(String content) {
-        return content.length() > 1
-                && content.charAt(content.length() - 1) == '\\'
-                && content.charAt(content.length() - 2) == '\\';
-    }
+        @Override
+        protected String format() {
+            boolean originCrossing = isCrossing;
+            int originFormulaCount = formulaCount;
 
-    private String processInterFormula(FileContext fileContext, LineIterator iterator, String content) {
-        int cursor = 0;
+            while (cursor < content.length()) {
 
-        StringBuilder sb = new StringBuilder();
+                // escape inner line code
+                if (content.charAt(cursor) == '`') {
+                    // '`' is not allowed in formula
+                    if (!isMatchLeft) {
+                        DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: '`' is not allowed in formula. line content: {}", file, content);
+                        throw new RuntimeException();
+                    }
 
-        while (cursor < content.length()) {
-            // escape inner line code
-            if (content.charAt(cursor) == '`') {
-                // '`' is not allowed in formula
-                if (!isInterFormulaStart) {
-                    DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: '`' is not allowed in formula. line content: {}", fileContext.getFile(), content);
-                    throw new RuntimeException();
-                }
+                    sb.append(content.charAt(cursor++));
 
-                sb.append(content.charAt(cursor++));
+                    while (content.charAt(cursor) != '`') {
+                        sb.append(content.charAt(cursor++));
+                    }
 
-                while (content.charAt(cursor) != '`') {
+                    sb.append(content.charAt(cursor++));
+                } else if (isBoundary()) {
+                    if (isMatchLeft) {
+                        if (!hasFormulaStart()) {
+                            sb.append(FORMULA_WRAPPER_START);
+                        }
+                        sb.append("$");
+                    } else {
+                        sb.append("$");
+                        if (!hasFormulaEnd()) {
+                            sb.append(FORMULA_WRAPPER_END);
+                        }
+                    }
+                    isMatchLeft = !isMatchLeft;
+                    formulaCount++;
+                    cursor++;
+                    isCrossing = false;
+                } else {
                     sb.append(content.charAt(cursor++));
                 }
-
-                sb.append(content.charAt(cursor++));
-            } else if (isInterFormulaBoundary(content, cursor)) {
-                if (isInterFormulaStart) {
-                    if (hasFormulaStart(content, cursor)) {
-                        sb.append("$$");
-                    } else {
-                        sb.append(FORMULA_WRAPPER_START)
-                                .append("$$");
-                    }
-                } else {
-                    if (hasFormulaEnd(content, cursor + 1)) {
-                        sb.append("$$");
-                    } else {
-                        sb.append("$$").append(FORMULA_WRAPPER_END);
-                    }
-                }
-                cursor += 2;
-                isInterFormulaStart = !isInterFormulaStart;
-                interFormulaCount++;
-            } else {
-                sb.append(content.charAt(cursor++));
             }
+
+            if (isCrossLine()) {
+                isCrossing = true;
+            } else if (!isMatchLeft && isCrossing) {
+                DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: Cross line inner formula must end with \\\\. line content: {}", file, content);
+                throw new RuntimeException();
+            }
+
+            if (originCrossing == isCrossing
+                    && (formulaCount - originFormulaCount & 1) != 0) {
+                DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: Inner formula is not match in one line. line content: {}", file, content);
+                throw new RuntimeException();
+            }
+
+            return sb.toString();
         }
 
-        return sb.toString();
+        private boolean isBoundary() {
+            if (content.charAt(cursor) == '$'
+                    && (cursor == content.length() - 1 || content.charAt(cursor + 1) != '$')) {
+                return isPrefixBoundary();
+            }
+            return false;
+        }
+
+        private boolean hasFormulaEnd() {
+            return hasFormulaEnd(cursor);
+        }
+
+        private boolean isCrossLine() {
+            return content.length() > 1
+                    && content.charAt(content.length() - 1) == '\\'
+                    && content.charAt(content.length() - 2) == '\\';
+        }
     }
 
-    private boolean isInterFormulaBoundary(String content, int cursor) {
-        if (cursor < content.length() - 1
-                && content.charAt(cursor) == '$'
-                && content.charAt(cursor + 1) == '$'
-                && (cursor == content.length() - 2 || content.charAt(cursor + 2) != '$')) {
-            if (cursor == 0) {
-                return true;
-            } else if (cursor == 1) {
-                return content.charAt(cursor - 1) != '\\' && content.charAt(cursor - 1) != '$';
-            } else {
-                return content.charAt(cursor - 1) != '\\' && !(content.charAt(cursor - 1) == '$' && content.charAt(cursor - 2) != '\\');
-            }
+    private static final class InterFormulaWrapper extends AbstractFormulaWrapper {
+        private static boolean isMatchLeft;
+        private static int formulaCount;
+
+        public InterFormulaWrapper(File file, String content) {
+            super(file, content);
         }
-        return false;
+
+        @Override
+        protected String format() {
+            while (cursor < content.length()) {
+                // escape inner line code
+                if (content.charAt(cursor) == '`') {
+                    // '`' is not allowed in formula
+                    if (!isMatchLeft) {
+                        DEFAULT_LOGGER.error("file [{}] contains wrong formula grammar: '`' is not allowed in formula. line content: {}", file, content);
+                        throw new RuntimeException();
+                    }
+
+                    sb.append(content.charAt(cursor++));
+
+                    while (content.charAt(cursor) != '`') {
+                        sb.append(content.charAt(cursor++));
+                    }
+
+                    sb.append(content.charAt(cursor++));
+                } else if (isBoundary()) {
+                    if (isMatchLeft) {
+                        if (!hasFormulaStart()) {
+                            sb.append(FORMULA_WRAPPER_START);
+                        }
+                        sb.append("$$");
+
+                    } else {
+                        sb.append("$$");
+                        if (!hasFormulaEnd()) {
+                            sb.append("$$").append(FORMULA_WRAPPER_END);
+                        }
+                    }
+                    cursor += 2;
+                    isMatchLeft = !isMatchLeft;
+                    formulaCount++;
+                } else {
+                    sb.append(content.charAt(cursor++));
+                }
+            }
+            return sb.toString();
+        }
+
+        private boolean hasFormulaEnd() {
+            return hasFormulaEnd(cursor + 1);
+        }
+
+        private boolean isBoundary() {
+            if (cursor < content.length() - 1
+                    && content.charAt(cursor) == '$'
+                    && content.charAt(cursor + 1) == '$'
+                    && (cursor == content.length() - 2 || content.charAt(cursor + 2) != '$')) {
+                return isPrefixBoundary();
+            }
+            return false;
+        }
     }
 }
