4bcffb7833882314e1c0047584a11a0aeed34dae
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-02-24 00:37:00 +0800

    2018-02-24 00:37:00

--- compile-principle/src/main/java/org/liuyehcf/grammar/rg/nfa/NfaBuildIterator.java
+++ compile-principle/src/main/java/org/liuyehcf/grammar/rg/nfa/NfaBuildIterator.java
@@ -68,6 +68,13 @@ class NfaBuildIterator {
         return symbols.get(index);
     }
 
+    private Symbol getNextSymbol() {
+        if (index + 1 < symbols.size()) {
+            return symbols.get(index + 1);
+        }
+        return null;
+    }
+
     private boolean hasNext() {
         return index < symbols.size();
     }
@@ -98,20 +105,6 @@ class NfaBuildIterator {
     private StackUnion popStackUnion() {
         if (unions.isEmpty()) return null;
         return unions.pop();
-    }
-
-    private void parallel(NfaClosure preNfaClosure, NfaClosure nextNfaClosure) {
-
-        NfaState startNfaStateOfPreNfaClosure = preNfaClosure.getStartNfaState();
-        NfaState startNfaStateOfNextNfaClosure = nextNfaClosure.getStartNfaState();
-
-        for (Symbol inputSymbol : startNfaStateOfNextNfaClosure.getAllInputSymbol()) {
-            for (NfaState nextNfaState : startNfaStateOfNextNfaClosure.getNextNfaStatesWithInputSymbol(inputSymbol)) {
-                startNfaStateOfPreNfaClosure.addInputSymbolAndNextNfaState(inputSymbol, nextNfaState);
-            }
-        }
-
-        preNfaClosure.getEndNfaStates().addAll(nextNfaClosure.getEndNfaStates());
     }
 
     private void pushParallel() {
@@ -298,9 +291,11 @@ class NfaBuildIterator {
     private void processWhenEncounteredLeftSmallParenthesis() {
         enterGroup();
 
-        pushCurNfaClosure();
-
-        buildEmptyNfaClosure();
+        // 如果出现了()这种情况，那么才特殊处理一下。不加这个条件将会出现多余的ε边
+        if (SymbolUtils._rightSmallParenthesis.equals(getNextSymbol())) {
+            pushCurNfaClosure();
+            buildNonOrdinaryNfaClosure();
+        }
 
         moveForward();
     }
@@ -345,7 +340,7 @@ class NfaBuildIterator {
                 assertTrue(thirdTopStackUnion.isNfaClosure());
                 if (thirdTopStackUnion.getNfaClosure().getGroup()
                         != topStackUnion.getNfaClosure().getGroup()) {
-                    // "[ab](([ab])|([01]))*"
+                    // case "((a)|(b))"，将parallel操作滞后到group变更后
                     unions.push(thirdTopStackUnion);
                     unions.push(secondTopStackUnion);
                     break;
@@ -388,11 +383,7 @@ class NfaBuildIterator {
         curNfaClosure = buildNfaClosureWithSymbols(Arrays.asList(getCurSymbol()));
     }
 
-    private void buildEmptyNfaClosure() {
-        curNfaClosure = buildNonOrdinaryNfaClosure();
-    }
-
-    private NfaClosure buildNonOrdinaryNfaClosure() {
+    private void buildNonOrdinaryNfaClosure() {
         NfaState startNfaState = new NfaState();
         List<NfaState> endNfaStates = new ArrayList<>();
 
@@ -400,10 +391,12 @@ class NfaBuildIterator {
         startNfaState.addInputSymbolAndNextNfaState(Symbol.EPSILON, startNfaState);
         endNfaStates.add(startNfaState);
 
-        return new NfaClosure(startNfaState, endNfaStates, getCurGroup());
+        curNfaClosure = new NfaClosure(startNfaState, endNfaStates, getCurGroup());
     }
 
     private NfaClosure buildNfaClosureWithSymbols(Collection<Symbol> symbols) {
+        assertFalse(symbols.isEmpty());
+
         NfaState startNfaState = new NfaState();
         List<NfaState> endNfaStates = new ArrayList<>();
 
@@ -429,7 +422,7 @@ class NfaBuildIterator {
         // 连接前:
         //      pre.S --*> Pre.E(1)              next.S --> next.N(1)
         //      pre.S --*> Pre.E(2)              next.S --> next.N(2)
-        //       ...                               ...
+        //            ...                               ...
         //      Pre.S --*> Pre.E(n)              next.S --> next.N(m)
         //
         //
@@ -453,8 +446,6 @@ class NfaBuildIterator {
                 endNfaStateOfPreNfaClosure.setReceive(group);
             }
 
-
-
             // 以下循环用于连接两个NfaClosure
             for (Symbol inputSymbol : startNfaStateOfNextNfaClosure.getAllInputSymbol()) {
                 for (NfaState nextNfaState : startNfaStateOfNextNfaClosure.getNextNfaStatesWithInputSymbol(inputSymbol)) {
@@ -464,6 +455,45 @@ class NfaBuildIterator {
         }
 
         preNfaClosure.setEndNfaStates(nextNfaClosure.getEndNfaStates());
+    }
+
+    private void parallel(NfaClosure preNfaClosure, NfaClosure nextNfaClosure) {
+
+        // 符号说明:
+        //      S: 开始节点
+        //      S.N(i): 开始节点的第i个后继节点
+        //      -->: 经过一步跳转
+        //
+        // 连接前:
+        //      pre.S --> Pre.N(1)              next.S --> next.N(1)
+        //      pre.S --> Pre.N(2)              next.S --> next.N(2)
+        //            ...                              ...
+        //      Pre.S --> Pre.N(n)              next.S --> next.N(m)
+        //
+        //
+        // 连接后: 将前一个NfaClosure的起始节点，连接到后一个NfaClosure的起始节点的所有后继节点
+        //      pre.S --> Pre.N(1)
+        //      pre.S --> Pre.N(2)
+        //            ...
+        //      Pre.S --> Pre.N(n)
+        //
+        //      pre.S --> next.N(1)
+        //      pre.S --> next.N(2)
+        //            ...
+        //      pre.S --> next.N(m)
+        //
+        // next.S节点，在连接后被移除了
+
+        NfaState startNfaStateOfPreNfaClosure = preNfaClosure.getStartNfaState();
+        NfaState startNfaStateOfNextNfaClosure = nextNfaClosure.getStartNfaState();
+
+        for (Symbol inputSymbol : startNfaStateOfNextNfaClosure.getAllInputSymbol()) {
+            for (NfaState nextNfaState : startNfaStateOfNextNfaClosure.getNextNfaStatesWithInputSymbol(inputSymbol)) {
+                startNfaStateOfPreNfaClosure.addInputSymbolAndNextNfaState(inputSymbol, nextNfaState);
+            }
+        }
+
+        preNfaClosure.getEndNfaStates().addAll(nextNfaClosure.getEndNfaStates());
     }
 
     private static class GroupUtil {
@@ -488,6 +518,11 @@ class NfaBuildIterator {
         }
     }
 
+    /**
+     * 栈元素，元素为NfaClosure，或者一个占位符
+     * 当nfaClosure不为空时，就持有了一个NfaClosure
+     * 当nfaClosure为空时，即遇到'|'符号，压入了一个占位符
+     */
     private class StackUnion {
         private NfaClosure nfaClosure;
 
