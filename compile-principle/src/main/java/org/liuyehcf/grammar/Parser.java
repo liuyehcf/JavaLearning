235c3a84700fb8ebc6d8377b0c88b27dfebf68ea
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-03-23 17:26:58 +0800

    编译原理单独开一个新的项目，因此这里删掉了

--- compile-principle/src/main/java/org/liuyehcf/grammar/Parser.java
+++
@@ -1,8 +0,0 @@
-package org.liuyehcf.grammar;
-
-public interface Parser extends GrammarHolder {
-    /**
-     * 给定字符串是否为当前文法的句子
-     */
-    boolean matches(String input);
-}
--- compile-principle/src/main/java/org/liuyehcf/grammar/core/definition/converter/GrammarConverterPipeline.java
+++
@@ -1,7 +0,0 @@
-package org.liuyehcf.grammar.core.definition.converter;
-
-import org.liuyehcf.grammar.core.definition.Grammar;
-
-public interface GrammarConverterPipeline {
-    Grammar convert(Grammar grammar);
-}
