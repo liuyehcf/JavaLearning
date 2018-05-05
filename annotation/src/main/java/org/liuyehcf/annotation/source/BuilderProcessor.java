09ded08e09e8d48cf9d240577d9effdc3c373509
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-05-05 21:24:48 +0800

    annotation修改

--- annotation/src/main/java/org/liuyehcf/annotation/source/BuilderProcessor.java
+++ annotation/src/main/java/org/liuyehcf/annotation/source/BuilderProcessor.java
@@ -6,7 +6,9 @@ import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
+import com.sun.tools.javac.util.*;
 
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
