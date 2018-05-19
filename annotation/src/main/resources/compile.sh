c9a3a787de029696e7295be03cc6949b7f9bc915
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-05-19 21:57:34 +0800

    修改脚本文件

--- annotation/src/main/resources/compile.sh
+++ annotation/src/main/resources/compile.sh
@@ -6,16 +6,26 @@ fi
 
 mkdir classes
 
+# tools.jar的路径
 TOOLS_PATH="${JAVA_HOME}/lib/tools.jar"
 
 # 编译Builder注解以及注解处理器
 javac -cp ${TOOLS_PATH} $(find ../java -name "*.java")  -d classes/
 
+# 统计文件 `META-INF/services/javax.annotation.processing.Processor` 的行数
+LINE_NUM=$(cat META-INF/services/javax.annotation.processing.Processor | wc -l)+1
+
+# 将文件 `META-INF/services/javax.annotation.processing.Processor` 中的内容合并成串，以','分隔
+PROCESSORS=$(cat META-INF/services/javax.annotation.processing.Processor | awk '{ { printf $0 } if(NR < '"$LINE_NUM"') { printf "," } }')
+
 # 编译UserDTO.java，通过-process参数指定注解处理器
-javac -cp classes:. -d classes -processor org.liuyehcf.annotation.source.processor.DataProcessor,org.liuyehcf.annotation.source.processor.NoArgsConstructorProcessor,org.liuyehcf.annotation.source.processor.AllArgsConstructorProcessor,org.liuyehcf.annotation.source.processor.BuilderProcessor UserDTO.java
+javac -cp classes:. -d classes -processor $PROCESSORS UserDTO.java
 
 # 反编译静态内部类
 javap -cp classes:. -p UserDTO$UserDTOBuilder
 
 # 运行UserDTO
 java -cp classes:. UserDTO
+
+# 删除目录
+rm -rf classes
