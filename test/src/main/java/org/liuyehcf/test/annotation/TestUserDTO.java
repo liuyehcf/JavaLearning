a81cc7e19eb041b9a1b5b1039bffe5ae71ffa7ec
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-05-19 15:57:00 +0800

    get方法完成，set方法尚未完成

--- test/src/main/java/org/liuyehcf/test/annotation/TestUserDTO.java
+++ test/src/main/java/org/liuyehcf/test/annotation/TestUserDTO.java
@@ -1,10 +1,12 @@
 package org.liuyehcf.test.annotation;
 
 import org.liuyehcf.annotation.source.annotation.Builder;
+import org.liuyehcf.annotation.source.annotation.Data;
 import org.liuyehcf.annotation.source.annotation.NoArgsConstructor;
 
-@Builder
-@NoArgsConstructor
+//@Builder
+@Data
+//@NoArgsConstructor
 public class TestUserDTO {
     private String firstName;
 
@@ -21,49 +23,17 @@ public class TestUserDTO {
         this.address = address;
     }
 
-    public static void main(String[] args) {
-        TestUserDTO.TestUserDTOBuilder builder = TestUserDTO.builder();
-
-        TestUserDTO userDTO = builder.firstName("辰枫")
-                .lastName("贺")
-                .age(25)
-                .address("杭州")
-                .build();
-
-        System.out.println(userDTO);
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+//    public static void main(String[] args) {
+//        TestUserDTO.TestUserDTOBuilder builder = TestUserDTO.builder();
+//
+//        TestUserDTO userDTO = builder.firstName("辰枫")
+//                .lastName("贺")
+//                .age(25)
+//                .address("杭州")
+//                .build();
+//
+//        System.out.println(userDTO);
+//    }
 
     @Override
     public String toString() {
