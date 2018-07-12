6e2cdbd6003d3dda7ed127528a2b235fca3db049
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-07-12 19:15:13 +0800

    spring-cloud-configserver-configclient

---
+++ spring-cloud/config-client/src/main/java/org/liuyehcf/spring/cloud/config/client/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package org.liuyehcf.spring.cloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenlu
+ * @date 2018/7/12
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}
---
+++ spring-cloud/config-server/src/main/java/org/liuyehcf/spring/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.liuyehcf.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author chenlu
+ * @date 2018/7/12
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
