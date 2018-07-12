02348ebd7eec315823839aeb3801520640ac0bdd
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-07-12 20:47:26 +0800

    完成config-server与config-client

--- spring-cloud/config-client/src/main/java/org/liuyehcf/spring/cloud/config/client/ConfigClientApplication.java
+++ spring-cloud/config-client/src/main/java/org/liuyehcf/spring/cloud/config/client/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package org.liuyehcf.spring.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chenlu
  * @date 2018/7/12
  */
-@SpringBootApplication
 @RestController
+@EnableEurekaClient
+@SpringBootApplication
+@RequestMapping("/demo/config")
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
-    String foo;
+    //获取配置中心的属性
+    @Value("${host}")
+    private String host;
 
-    @RequestMapping(value = "/hi")
-    public String hi() {
-        return foo;
+    //获取配置中心的属性
+    @Value("${description}")
+    private String description;
+
+    @GetMapping("/getHost")
+    public String getHost() {
+        return this.host;
+    }
+
+    @GetMapping("/getDescription")
+    public String getDescription() {
+        return this.description;
     }
 }
--- spring-cloud/config-server/src/main/java/org/liuyehcf/spring/cloud/config/server/ConfigServerApplication.java
+++ spring-cloud/config-server/src/main/java/org/liuyehcf/spring/cloud/config/server/ConfigServerApplication.java
@@ -3,14 +3,16 @@ package org.liuyehcf.spring.cloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenlu
  * @date 2018/7/12
  */
 
-@SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
