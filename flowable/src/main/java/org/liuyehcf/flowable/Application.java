5d4f1957a1ef0c616088a90d50f3c52e863762ca
chenfeng.hcf <chenfeng.hcf@alibaba-inc.com>
2018-09-20 17:47:30 +0800

    修改注解

--- flowable/src/main/java/org/liuyehcf/flowable/Application.java
+++ flowable/src/main/java/org/liuyehcf/flowable/Application.java
@@ -2,14 +2,12 @@ package org.liuyehcf.flowable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author hechenfeng
  * @date 2018/7/25
  */
-@SpringBootApplication
-@ComponentScan(basePackages = "org.liuyehcf.flowable")
+@SpringBootApplication(scanBasePackages = "org.liuyehcf.flowable")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
