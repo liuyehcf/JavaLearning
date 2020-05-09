d2106e4fac352dbfc296ecf58015c109c8f52ba2
ChenfengHe <1559500551@qq.com>
2020-05-09 22:26:03 +0800

    隐藏accessKey

--- aliyun/src/main/java/org/liuyehcf/aliyun/AccessUtils.java
+++ aliyun/src/main/java/org/liuyehcf/aliyun/AccessUtils.java
@@ -4,7 +4,7 @@ package org.liuyehcf.aliyun;
  * Created by Liuye on 2017/12/15.
  */
 public class AccessUtils {
-    public static final String ACCESS_KEY = "LTAIROQDd1PQvWnk";
+    public static final String ACCESS_KEY = "xxx";
 
     public static final String ACCESS_SECRET_KEY = "xxx";
 
